82fe7f3554b824c58b51406c7a960144835950b2
lucasrengel <lucas_rengel@estudante.sc.senai.br>
2024-07-24 20:51:32 -0300

    feat: implementar metodo para a conexao com banco de dados

--- src/main/java/com/lucasrengel/dao/Conexao.java
+++ src/main/java/com/lucasrengel/dao/Conexao.java
@@ -1,5 +1,46 @@
 package com.lucasrengel.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexao {
-    
+
+    public static Connection getConexao() {
+
+        Connection connection = null;//instancia da conexao
+
+        try {
+            //carregando o driver jdbc
+            String driver = "com.mysql.cj.jdbc.Driver";
+            Class.forName(driver);
+
+            //configuracao do caminho do mySQL
+            String server = "localhost";
+            String database = "db_a3";
+            String url = "jdbc:mysql://" + server + ":3306/" + database;
+            String user = "root";
+            String password = "1234";
+
+            connection = DriverManager.getConnection(url, user, password);
+
+            // testando
+            if (connection != null) {
+                System.out.println("Status: Conectado!");
+            } else {
+                System.out.println("Status: NAO CONECTADO!");
+            }
+
+            return connection;
+
+        } catch (ClassNotFoundException e) {  //Driver nao encontrado
+            System.out.println("O driver nao foi encontrado. " + e.getMessage());
+            return null;
+
+        } catch (SQLException e) {
+            System.out.println("Nao foi possivel conectar...");
+            return null;
+        }
+
+    }
 }
