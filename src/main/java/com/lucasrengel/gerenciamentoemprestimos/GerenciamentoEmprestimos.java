b1cc53fc7f9e81733d7c9574273df551b12fdc65
lucasrengel <lucas_rengel@estudante.sc.senai.br>
2024-07-24 19:57:26 -0300

    Inicio do projeto

---
+++ src/main/java/com/lucasrengel/gerenciamentoemprestimos/GerenciamentoEmprestimos.java
@@ -0,0 +1,8 @@
+package com.lucasrengel.gerenciamentoemprestimos;
+
+public class GerenciamentoEmprestimos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
