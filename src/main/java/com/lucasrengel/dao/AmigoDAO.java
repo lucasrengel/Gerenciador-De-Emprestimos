5755a9270ee122fb56922c4f89b12dccc781dbee
lucasrengel <lucas_rengel@estudante.sc.senai.br>
2024-07-27 23:53:02 -0300

    feat: adicionar funcoes de conexao nas entidades dao

--- src/main/java/com/lucasrengel/dao/AmigoDAO.java
+++ src/main/java/com/lucasrengel/dao/AmigoDAO.java
@@ -1,10 +1,16 @@
 package com.lucasrengel.dao;
 
 import com.lucasrengel.modelo.Amigo;
+import java.sql.Connection;
 import java.util.ArrayList;
 
 // Classe DAO (Data Access Object) para a entidade Amigo, contendo métodos para realizar operações de CRUD no banco de dados.
 public class AmigoDAO {
 
     public static ArrayList<Amigo> minhaLista = new ArrayList<>();
+
+    //metodo para se conectar ao banco de dados
+    private Connection getConexao() {
+        return Conexao.getConexao();
+    }
 }
--- src/main/java/com/lucasrengel/dao/FerramentaDAO.java
+++ src/main/java/com/lucasrengel/dao/FerramentaDAO.java
@@ -1,9 +1,16 @@
 package com.lucasrengel.dao;
 
 import com.lucasrengel.modelo.Ferramenta;
+import java.sql.Connection;
 import java.util.ArrayList;
 
 // Classe DAO (Data Access Object) para a entidade Ferramenta, contendo métodos para realizar operações de CRUD no banco de dados.
 public class FerramentaDAO {
+
     public static ArrayList<Ferramenta> minhaLista = new ArrayList<>();
+
+    //metodo para se conectar ao banco de dados
+    private Connection getConexao() {
+        return Conexao.getConexao();
+    }
 }
