e7eb57352728f95008efa0bb5728305c60673338
lucasrengel <lucas_rengel@estudante.sc.senai.br>
2024-08-09 23:46:14 -0300

    feat: adicionar metodo para retornar array de amigo

--- src/main/java/com/lucasrengel/dao/AmigoDAO.java
+++ src/main/java/com/lucasrengel/dao/AmigoDAO.java
@@ -2,12 +2,41 @@ package com.lucasrengel.dao;
 
 import com.lucasrengel.modelo.Amigo;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 // Classe DAO (Data Access Object) para a entidade Amigo, contendo métodos para realizar operações de CRUD no banco de dados.
 public class AmigoDAO {
 
     public static ArrayList<Amigo> minhaLista = new ArrayList<>();
+    
+     //retorna a lista de amigos
+    public ArrayList getMinhaLista() {
+
+        minhaLista.clear();
+
+        try {
+            Statement stmt = this.getConexao().createStatement();
+            ResultSet res = stmt.executeQuery("SELECT * FROM tb_amigos");
+            while (res.next()) {
+
+                int id = res.getInt("id");
+                String nome = res.getString("nome");
+                String telefone = res.getString("telefone");
+
+                Amigo objeto = new Amigo(id, nome, telefone);
+
+                minhaLista.add(objeto);
+            }
+
+            stmt.close();
+
+        } catch (SQLException ex) {
+        }
+
+        return minhaLista;
+    }
 
     //metodo para se conectar ao banco de dados
     private Connection getConexao() {
