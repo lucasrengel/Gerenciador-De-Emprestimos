2dbfb7221e080bb4f921108c6a6ee9abd52032b6
lucasrengel <lucas_rengel@estudante.sc.senai.br>
2024-08-12 23:33:19 -0300

    feat: adicionar metodo insert amigo

--- src/main/java/com/lucasrengel/dao/AmigoDAO.java
+++ src/main/java/com/lucasrengel/dao/AmigoDAO.java
@@ -1,10 +1,8 @@
 package com.lucasrengel.dao;
 
 import com.lucasrengel.modelo.Amigo;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+
+import java.sql.*;
 import java.util.ArrayList;
 
 // Classe DAO (Data Access Object) para a entidade Amigo, contendo métodos para realizar operações de CRUD no banco de dados.
@@ -37,6 +35,27 @@ public class AmigoDAO {
         }
 
         return minhaLista;
+    }
+
+    //cadastra um novo amigo com o objeto criado
+    public boolean insertAmigoBD(Amigo objeto) {
+        String sql = "INSERT INTO tb_amigos(id,nome,telefone) VALUES(?,?,?)";
+
+        try {
+            PreparedStatement stmt = this.getConexao().prepareStatement(sql);
+
+            stmt.setInt(1, objeto.getId());
+            stmt.setString(2, objeto.getNome());
+            stmt.setString(3, objeto.getTelefone());
+
+            stmt.execute();
+            stmt.close();
+
+            return true;
+
+        } catch (SQLException erro) {
+            throw new RuntimeException(erro);
+        }
     }
 
     //metodo para se conectar ao banco de dados
