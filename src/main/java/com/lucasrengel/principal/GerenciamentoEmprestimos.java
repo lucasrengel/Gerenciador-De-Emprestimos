946239a0c12285547eb30ed297e75eddd979a8a4
lucasrengel <lucas_rengel@estudante.sc.senai.br>
2024-08-18 18:03:29 -0300

    fix: altera tema para nimbus

--- src/main/java/com/lucasrengel/principal/GerenciamentoEmprestimos.java
+++ src/main/java/com/lucasrengel/principal/GerenciamentoEmprestimos.java
@@ -5,7 +5,27 @@ import com.lucasrengel.visao.TelaMenu;
 public class GerenciamentoEmprestimos {
 
     public static void main(String[] args) {
-        TelaMenu tela = new TelaMenu();
-        tela.setVisible(true);
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(TelaMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(TelaMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(TelaMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(TelaMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new TelaMenu().setVisible(true);
+            }
+        });
     }
 }
