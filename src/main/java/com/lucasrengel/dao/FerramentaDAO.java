003f3569cc9f966ecd4c7dd1f2b69c17c35a424f
lucasrengel <lucas_rengel@estudante.sc.senai.br>
2024-08-29 19:29:59 -0300

    feat: adiciona metodo para verificar se ferramenta ja foi emprestada

--- src/main/java/com/lucasrengel/dao/FerramentaDAO.java
+++ src/main/java/com/lucasrengel/dao/FerramentaDAO.java
@@ -138,6 +138,22 @@ public class FerramentaDAO {
         return precoTotal;
     }
 
+    public boolean isFerramentaEmprestada(int ferramentaId) {
+        try {
+            PreparedStatement stmt = this.getConexao().prepareStatement("SELECT COUNT(*) FROM tb_emprestimos WHERE id_ferramenta = ? AND data_devolucao IS NULL");
+            stmt.setInt(1, ferramentaId);
+            ResultSet res = stmt.executeQuery();
+
+            res.next();
+            int count = res.getInt(1);
+
+            stmt.close();
+            return count > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     //metodo para se conectar ao banco de dados
     private Connection getConexao() {
         return Conexao.getConexao();
